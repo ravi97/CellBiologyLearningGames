d2b5fe037b6b6d9780bc001c252b8a70d0514429
Ravikiran <raviramesh.kiran97@gmail.com>
2020-04-04 10:30:58 -0500

    exported all game data to a seperate js file for SCRAMBLE game

---
+++ Scramble/js/game_data.js
@@ -0,0 +1,39 @@
+//insert a word into the easy word bank by adding another row
+//inside the brackets of the variable 'levelEasy'
+//in the format <word>:"<definition>",
+var levelEasy = {
+  ribosomes: "Non-membrane bound structures which make proteins",
+  lysosome: "Fluid-filled bags containing digestive chemicals",
+  chloroplast: "Energy producing organelle found in all plant cells",
+  chlorophyll: "Green pigment which gives plants their color",
+  vacuole: "Storage bubbles within a cell which may contain water, nutrients, or waste",
+  tissue: "Different types of cells performing specialized functions",
+  organs: "Two or more different types of tissue working together",
+};
+
+//insert a word into the easy word bank by adding another row
+//inside the brackets of the variable 'levelMedium'
+//in the format <word>:"<definition>",
+var levelMedium = {
+  phospholipid: "A bilayer that makes up the plasma membrane",
+  cytosol: "Gel-like fluid that fills the cell and holds organelles in place",
+  cytosckeleton: "A network of microtubules that support the cell's shape",
+  microtubules: "Make up the cytosckeleton",
+  organelles: "Little organs",
+  nucleus: "A spherical membrane-bound organelle that holds the cell's DNA",
+  nucleolus: "Dense, protein-rish region in the nucleus",
+};
+
+//insert a word into the easy word bank by adding another row
+//inside the brackets of the variable 'levelHard'
+//in the format <word>:"<definition>",
+var levelHard = {
+  mitochondria: "The powerhouse of the cell",
+  prokaryotic: "Cells with no membrane-bound organelles",
+  eukaryotic: "Cells with membrane-bound organelles",
+  flagella: "A tail that whips back and forth for movement",
+  pili: "Help prokaryotic cells hold onto surfaces",
+  cytoplasm: "A cell's fluids",
+  hydrophilic: "Water loving",
+  hydrophobic: "Water fearing",
+};
--- Scramble/js/script.js
+++ Scramble/js/script.js
@@ -9,34 +9,6 @@
 
 //})
 
-var levelEasy = {
-  ribosomes: "Non-membrane bound structures which make proteins",
-  lysosome: "Fluid-filled bags containing digestive chemicals",
-  chloroplast: "Energy producing organelle found in all plant cells",
-  chlorophyll: "Green pigment which gives plants their color",
-  vacuole: "Storage bubbles within a cell which may contain water, nutrients, or waste",
-  tissue: "Different types of cells performing specialized functions",
-  organs: "Two or more different types of tissue working together"
-};
-var levelMedium = {
-  phospholipid: "A bilayer that makes up the plasma membrane",
-  cytosol: "Gel-like fluid that fills the cell and holds organelles in place",
-  cytosckeleton: "A network of microtubules that support the cell's shape",
-  microtubules: "Make up the cytosckeleton",
-  organelles: "Little organs",
-  nucleus: "A spherical membrane-bound organelle that holds the cell's DNA",
-  nucleolus: "Dense, protein-rish region in the nucleus"
-};
-var levelHard = {
-  mitochondria: "The powerhouse of the cell",
-  prokaryotic: "Cells with no membrane-bound organelles",
-  eukaryotic: "Cells with membrane-bound organelles",
-  flagella: "A tail that whips back and forth for movement",
-  pili: "Help prokaryotic cells hold onto surfaces",
-  cytoplasm: "A cell's fluids",
-  hydrophilic: "Water loving",
-  hydrophobic: "Water fearing"
-};
 var level;
 var wordbank;
 var rndNum;
