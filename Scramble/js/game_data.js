1ea6673a041407dd823c95983d5f8baa87be26c4
Ravikiran <raviramesh.kiran97@gmail.com>
2020-04-29 11:44:10 -0500

    updated the game data of SCRAMBLE game

--- Scramble/js/game_data.js
+++ Scramble/js/game_data.js
@@ -3,37 +3,38 @@
 //in the format <word>:"<definition>",
 var levelEasy = {
   ribosomes: "Non-membrane bound structures which make proteins",
-  lysosome: "Fluid-filled bags containing digestive chemicals",
-  chloroplast: "Energy producing organelle found in all plant cells",
-  chlorophyll: "Green pigment which gives plants their color",
-  vacuole: "Storage bubbles within a cell which may contain water, nutrients, or waste",
-  tissue: "Different types of cells performing specialized functions",
-  organs: "Two or more different types of tissue working together",
+  lysosome: "fluid-filled ‘bags’ containing digestive chemicals for breaking down food molecules, cell wastes, and worn-out cell parts",
+  chloroplast: "Energy-producing organelle containing Chlorophyll (green pigment the site of photosynthesis) and found in all plant cells and some bacteria",
+  organism: "organ systems working together to form a fully functional living being that can thrive in a particular environment",
+  vacuole: "an enclosed area within plant cells that holds liquid",
+  tissue: "many types of cells working together to perform a specific function",
+  organ: "two or more different types of tissue working together to perform a specific function",
 };
 
 //insert a word into the easy word bank by adding another row
 //inside the brackets of the variable 'levelMedium'
 //in the format <word>:"<definition>",
 var levelMedium = {
-  phospholipid: "A bilayer that makes up the plasma membrane",
-  cytosol: "Gel-like fluid that fills the cell and holds organelles in place",
-  cytosckeleton: "A network of microtubules that support the cell's shape",
-  microtubules: "Make up the cytosckeleton",
-  organelles: "Little organs",
-  nucleus: "A spherical membrane-bound organelle that holds the cell's DNA",
-  nucleolus: "Dense, protein-rish region in the nucleus",
+  cell_wall: "tough, rigid outer covering that protects and provides shape to the cell",
+  cytosol: "gel-like fluid that fills the cell, holds organelles in place, and aids in waste break down and energy transformation (metabolism)",
+  cytoskeleton: "a vast network of proteins (called microtubules) that supports the cell’s shape like a skeleton and along which organelles and vesicles to travel",
+  organ_system: "organs working together to perform certain functions",
+  organelle: "specialized subunit within cells that performs specific functions",
+  plasma_membrane:"membrane of lipids and proteins that forms the external boundary of a cell and serves as a gate keeper letting only select things in and out",
+  nucleus: "spherical membrane-bound organelle that holds all the genetic material of a cell",
+  nucleolus: "dense, protein-rich region in the nucleus; produces subunits that form ribosomes",
 };
 
 //insert a word into the easy word bank by adding another row
 //inside the brackets of the variable 'levelHard'
 //in the format <word>:"<definition>",
 var levelHard = {
-  mitochondria: "The powerhouse of the cell",
-  prokaryotic: "Cells with no membrane-bound organelles",
-  eukaryotic: "Cells with membrane-bound organelles",
-  flagella: "A tail that whips back and forth for movement",
-  pili: "Help prokaryotic cells hold onto surfaces",
-  cytoplasm: "A cell's fluids",
-  hydrophilic: "Water loving",
-  hydrophobic: "Water fearing",
+  mitochondria: "site of respiration (energy production) where chemical energy in food is transformed into ATP—the energy of cells ",
+  prokaryotic: "cells that lack membrane-bound nuclei or organelles",
+  eukaryotic: "cells with membrane-bound nuclei and other organelles",
+  golgi_apparatus: "membrane-bound organelle of flattened stacks where proteins are processed, packaged, and sent out as vesicles",
+  rough_endoplasmic_reticulum: "membrane-bound organelle studded with ribosomes which modify proteins",
+  cytoplasm: "all the contents inside a cell",
+  photosynthesis: "process in which plants covert solar energy into chemical energy in the form of sugar (glucose)",
+  smooth_endoplasmic_reticulum: "membrane-bound organelle not studded with ribosomes which modify proteins",
 };
