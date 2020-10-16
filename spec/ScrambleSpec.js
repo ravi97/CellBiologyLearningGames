ea663c81cc03caab720f4ac59240fde2993f2928
Jagriff <jagriffin@tamu.edu>
2020-10-16 11:09:33 -0500

    jasmine working

---
+++ spec/ScrambleSpec.js
@@ -0,0 +1,8 @@
+describe("Scramble game", function() {
+   describe("when player plays game", function(){
+      it("should be able to play a scramble game", function() {
+         expect(true).toBe(true);
+      });
+   });
+});
+
