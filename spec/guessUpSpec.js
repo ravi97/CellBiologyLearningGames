db7941e884066597547f4aafe6735548c579e70d
JainRohitLive <rohitjain@tamu.edu>
2020-10-17 23:10:05 -0500

    Revert "Complete cleaning of guess.js and getting guessUpSpec to pass jasmine tests"
    This reverts commit 50585372432bde072d17ffeceaabe6e0d6dd2749.

--- spec/guessUpSpec.js
+++ spec/guessUpSpec.js
@@ -1,7 +1,8 @@
 describe("guessGame", function() {
    describe("when player plays game", function(){
       it("should be able to play a guess game", function() {
-         expect(true).toBe(true);
+         play();
       });
    });
 });
+
